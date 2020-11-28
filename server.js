66fb34915f01227e20b9b6781ce3321cc2f0311a
Bishesh <bishesh.sunam@gmail.com>
2020-11-28 21:19:00 +1300

    axios req to twitter api

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res)=>{
+
+
+
+
+
+app.get('/tweets', (req, res)=>{
+    console.log(req.query)
+    const query = req.query.q;
+    const count = req.query.count;
+    const url = "https://api.twitter.com/1.1/search/tweets.json";
+    axios.get(url, {
+        params: {
+            q: query,
+            count: count
+        },
+        headers: {
+            "Authorization": "AAAAAAAAAAAAAAAAAAAAAPilJgEAAAAA67Wy01kjKkmwqDsr1mOK5YuBri4%3DF8NrnTX0w778boRKL2DFxRwLAk0EWuBNfsb0o9qNzzoY8PaI63"
+        }
+    }).then ((response)=> {
+        res.status(200).send(response.data)
+    }).catch((err)=>{
+       res.status(400).send(err);
+    })
+    
+
     res.send("Hello world")
 })
 
